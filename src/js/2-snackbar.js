4747318cca70b47560d08b40baeb9e3875cd263d
Oleksandr-Kalnaus <oleksandrkalnaus@gmail.com>
2024-08-09 13:37:32 +0100

    Edit task 2 - snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,49 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const form = document.querySelector(`.form`);
 
+form.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+
+    const delay = parseInt(form.delay.value, 10);
+    const state = form.state.value;
+
+    createNotification(delay, state);
+});
+
+function createNotification(delay, state) {
+    const notificationPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+                if (delay < 0) {
+                    iziToast.error({
+                        title: `Error`,
+                        message: `Delay must be a positive number.`,
+                        position: 'topRight',
+                    });
+                    return;
+    }
+            else if (state === 'fulfilled') {
+                resolve(`✅ Fulfilled promise in ${delay}ms`);
+            } else {
+                reject(`❌ Rejected promise in ${delay}ms`);
+            }
+        }, delay);
+    });
+
+    notificationPromise
+        .then((message) => {
+            iziToast.success({
+                title: `OK`,
+                message: message,
+                position: 'topRight',
+            });
+        })
+        .catch((message) => {
+            iziToast.error({
+                title: `Not OK`,
+                message: message,
+                position: 'topRight',
+            });
+        });
+}
